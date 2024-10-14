1c39877648e8979a02fcffaacf53d02dc6f34278
paulovitorss2013 <paulovitor_santos@hotmail.com.br>
2024-10-14 16:33:57 -0300

    Projeto Angular 17 limpo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,19 +1,10 @@
 import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  templateUrl: 'app.component.html',
-  standalone: true,
-  imports: [CommonModule, RouterOutlet],
-  template: `
-    <h1>Welcome to {{title}}!</h1>
-
-    <router-outlet></router-outlet>
-  `,
-  styles: [],
+  templateUrl: 'app.component.html'
+     
 })
 export class AppComponent {
-  title = 'onlikeos';
+  title = 'onlike-os';
 }
